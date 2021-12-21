458ae4ce68ced11e320dae3702dca6cd4ca0591f
brillant <jbrillantf@gmail.com>
2021-12-21 19:46:15 +0300

    Chat commune mis au point

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -2,6 +2,6 @@ const userCtrl = require('../controlers/user.controler');
 const router = require('express').Router();
 
 router.post('/signup', userCtrl.signup);
-router.use('/login', userCtrl.login);
+router.post('/login', userCtrl.login);
 
 module.exports = router;
